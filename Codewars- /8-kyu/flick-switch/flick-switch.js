748e92c17fbccf2ec78c504a65c546ad00eea25a
Mahad Ahmed Farah <mahadahmed3528@gmail.com>
2026-06-08 19:37:25 +0200

    Add solution - CodeHub

---
+++ Codewars- /8-kyu/flick-switch/flick-switch.js
@@ -0,0 +1,13 @@
+function flickSwitch(arr) {
+  let state = true;
+  let result = [];
+​
+  for (let item of arr) {
+    if (item === "flick") {
+      state = !state; // switch true ↔ false
+    }
+    result.push(state);
+  }
+​
+  return result;
+}
