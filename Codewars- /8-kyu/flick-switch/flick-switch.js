dbf1b29a3bc49b09672578fe3675b31d873fa23f
Mahad Ahmed Farah <mahadahmed3528@gmail.com>
2026-06-26 19:11:10 +0200

    Refactor flickSwitch function and fix formatting
    Fix formatting and add newline at end of file.

--- Codewars- /8-kyu/flick-switch/flick-switch.js
+++ Codewars- /8-kyu/flick-switch/flick-switch.js
@@ -11,3 +11,16 @@ function flickSwitch(arr) {
 ​
   return result;
 }
+function flickSwitch(arr) {
+  let state = true;
+  let result = [];
+​
+  for (let item of arr) {
+    if (item === "flick") {
+      state = !state; // switch true ↔ false
+    }
+    result.push(state);
+  }
+​
+  return result;
+}
