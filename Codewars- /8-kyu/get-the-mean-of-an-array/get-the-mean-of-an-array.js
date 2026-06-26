de7f1a74c8746eb8cee7fb4983c6740d94190c5c
Mahad Ahmed Farah <mahadahmed3528@gmail.com>
2026-06-26 19:11:58 +0200

    Update get-the-mean-of-an-array.js

--- Codewars- /8-kyu/get-the-mean-of-an-array/get-the-mean-of-an-array.js
+++ Codewars- /8-kyu/get-the-mean-of-an-array/get-the-mean-of-an-array.js
@@ -16,3 +16,12 @@ for(let i=0; i<marks.length; i++) {
   
     return  Math.floor(sum / marks.length);
 };
+function getAverage(marks){
+  //TODO : calculate the downward rounded average of the marks array
+  let sum =0;
+for(let i=0; i<marks.length; i++) {
+    sum += marks[i];
+  };
+  
+    return  Math.floor(sum / marks.length);
+};
