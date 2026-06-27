c1a5a85fea690a57af6bd0176974981a21c4db47
Mahad Ahmed Farah <mahadahmed3528@gmail.com>
2026-06-27 17:49:23 +0200

    Update training-js-number-5-basic-data-types-object.js

--- Codewars- /8-kyu/training-js-number-5-basic-data-types-object/training-js-number-5-basic-data-types-object.js
+++ Codewars- /8-kyu/training-js-number-5-basic-data-types-object/training-js-number-5-basic-data-types-object.js
@@ -2,4 +2,8 @@ function animal(obj){
   return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
 }
 ​
+​function animal(obj){
+  return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+}
+​
 ​
