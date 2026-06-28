0f4066e30766ab62ef5c7437486da6460f6d0a15
Mahad Ahmed Farah <mahadahmed3528@gmail.com>
2026-06-28 22:38:16 +0200

    Update how-old-will-i-be-in-2099.js

--- Codewars- /8-kyu/how-old-will-i-be-in-2099/how-old-will-i-be-in-2099.js
+++ Codewars- /8-kyu/how-old-will-i-be-in-2099/how-old-will-i-be-in-2099.js
@@ -23,7 +23,22 @@ function  calculateAge(birthYear, year) {
     const yearWord= age === 1? "year":"years"
      if(year > birthYear){
     return  `You are ${age} ${yearWord} old.`;
+  } function  calculateAge(birthYear, year) {  
+  // enter your code here.
+    const age = Math.abs(year - birthYear);
+​
+  if(birthYear === year){
+    return "You were born this very year!";
   } 
+    const yearWord= age === 1? "year":"years"
+     if(year > birthYear){
+    return  `You are ${age} ${yearWord} old.`;
+  } 
+    return `You will be born in ${age} ${yearWord}.`;
+  };
+​
+​
+​
     return `You will be born in ${age} ${yearWord}.`;
   };
 ​
