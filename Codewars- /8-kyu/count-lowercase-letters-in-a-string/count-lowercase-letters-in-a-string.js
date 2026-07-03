c16164e9f9a27193349972fe1a6147110e768ac9
Mahad Ahmed Farah <mahadahmed3528@gmail.com>
2026-07-03 14:13:13 +0200

    Add lowercaseCount function to count lowercase letters

--- Codewars- /8-kyu/count-lowercase-letters-in-a-string/count-lowercase-letters-in-a-string.js
+++ Codewars- /8-kyu/count-lowercase-letters-in-a-string/count-lowercase-letters-in-a-string.js
@@ -61,3 +61,12 @@ function lowercaseCount(str){
   }
   return count;
 }
+function lowercaseCount(str){
+    //How many?
+  let count = 0;
+  for(let char of str){
+    if(char >= 'a' && char <= 'z')
+      count++
+  }
+  return count;
+}
