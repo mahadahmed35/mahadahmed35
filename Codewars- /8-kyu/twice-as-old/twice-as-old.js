d68d196d034a79ca0ae75070f4026a0cb99d8d95
Mahad Ahmed Farah <mahadahmed3528@gmail.com>
2026-07-04 23:17:12 +0200

    Fix duplicate function declaration in twice-as-old.js

--- Codewars- /8-kyu/twice-as-old/twice-as-old.js
+++ Codewars- /8-kyu/twice-as-old/twice-as-old.js
@@ -22,3 +22,7 @@ function twiceAsOld(dadYearsOld, sonYearsOld) {
   // your code here
   return Math.abs(dadYearsOld-2*sonYearsOld);
 }
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  // your code here
+  return Math.abs(dadYearsOld-2*sonYearsOld);
+}
