9431b80c2c172bce69a8df585d846585bc5f6230
Mahad Ahmed Farah <mahadahmed3528@gmail.com>
2026-07-04 23:15:59 +0200

    Fix formatting and duplicate function definition

--- Codewars- /8-kyu/twice-as-old/twice-as-old.js
+++ Codewars- /8-kyu/twice-as-old/twice-as-old.js
@@ -6,3 +6,7 @@ function twiceAsOld(dadYearsOld, sonYearsOld) {
   // your code here
   return Math.abs(dadYearsOld-2*sonYearsOld);
 }
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  // your code here
+  return Math.abs(dadYearsOld-2*sonYearsOld);
+}
