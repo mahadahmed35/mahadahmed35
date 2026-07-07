1016af9af4d9641c553f06df75d51da98a6c2c95
Mahad Ahmed Farah <mahadahmed3528@gmail.com>
2026-07-07 15:52:11 +0200

    Fix duplicate animal function definition

--- Codewars- /8-kyu/training-js-number-5-basic-data-types-object/training-js-number-5-basic-data-types-object.js
+++ Codewars- /8-kyu/training-js-number-5-basic-data-types-object/training-js-number-5-basic-data-types-object.js
@@ -47,3 +47,7 @@ function animal(obj){
   return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
 }
 ​
+function animal(obj){
+  return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+}
+​
