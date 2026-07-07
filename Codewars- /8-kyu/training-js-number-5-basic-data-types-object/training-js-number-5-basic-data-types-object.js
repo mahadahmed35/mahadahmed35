8b9ae0514ebc76bcc47302bfbf9df91ee901c535
Mahad Ahmed Farah <mahadahmed3528@gmail.com>
2026-07-07 15:51:59 +0200

    Fix formatting of animal function

--- Codewars- /8-kyu/training-js-number-5-basic-data-types-object/training-js-number-5-basic-data-types-object.js
+++ Codewars- /8-kyu/training-js-number-5-basic-data-types-object/training-js-number-5-basic-data-types-object.js
@@ -43,4 +43,7 @@ function animal(obj){
 function animal(obj){
   return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
 }
+​function animal(obj){
+  return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+}
 ​
