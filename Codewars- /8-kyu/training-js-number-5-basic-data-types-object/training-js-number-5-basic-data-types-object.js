a69dec2e56c22fd5702bdd98c437cd28e829cd8a
Mahad Ahmed Farah <mahadahmed3528@gmail.com>
2026-07-07 15:52:37 +0200

    Fix formatting and remove duplicate animal function

--- Codewars- /8-kyu/training-js-number-5-basic-data-types-object/training-js-number-5-basic-data-types-object.js
+++ Codewars- /8-kyu/training-js-number-5-basic-data-types-object/training-js-number-5-basic-data-types-object.js
@@ -54,3 +54,7 @@ function animal(obj){
   return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
 }
 ​
+function animal(obj){
+  return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+}
+​
