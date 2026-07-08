fe1e8086b95256f550755b343a90234972df791f
Mahad Ahmed Farah <mahadahmed3528@gmail.com>
2026-07-08 13:41:55 +0200

    Add solution - CodeHub

---
+++ Codewars- /8-kyu/welcome/welcome.js
@@ -0,0 +1,34 @@
+      return "Bienvenue";
+      break;
+      case "german":
+      return "Willkommen";
+      break;
+      case "irish":
+      return "Failte";
+      break;
+      case "italian":
+      return "Benvenuto";
+      break;
+      case "latvian":
+      return "Gaidits";
+      break;
+      case "lithuanian":
+      return "Laukiamas";
+      break;
+      case "polish":
+      return "Witamy";
+      break;
+      case "spanish":
+      return "Bienvenido";
+      break;
+      case "swedish":
+      return "Valkommen";
+      break;
+      case "welsh":
+      return "Croeso";
+      break;
+    default : 
+      return "Welcome"; 
+    
+  }
+}
