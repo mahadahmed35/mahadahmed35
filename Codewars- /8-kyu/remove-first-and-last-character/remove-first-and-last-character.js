61a9aee86b8f51192d35d507f8663e89bdcf2864
Mahad Ahmed Farah <mahadahmed3528@gmail.com>
2026-07-09 17:17:17 +0200

    Update solution - CodeHub

--- Codewars- /8-kyu/remove-first-and-last-character/remove-first-and-last-character.js
+++ Codewars- /8-kyu/remove-first-and-last-character/remove-first-and-last-character.js
@@ -5,42 +5,4 @@ return str.slice(1, -1)
 ​
 ​
 ​
-​function removeChar(str){
- //You got this!
-return str.slice(1, -1)
-};
-function removeChar(str){
- //You got this!
-return str.slice(1, -1)
-};
-​function removeChar(str){
- //You got this!
-return str.slice(1, -1)
-};
-function removeChar(str){
- //You got this!
-return str.slice(1, -1)
-};
-function removeChar(str){
- //You got this!
-return str.slice(1, -1)
-};
-​
-​
-​
-​
-​
-​
-​
-​
-​
-​
-​
-​
-​
-​
-​
-​
-​
-​
 ​
